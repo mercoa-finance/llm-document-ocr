047a6ba643d3e19b86d1137a3fd18c2eca433e82
Sandeep Dinesh <sandeep@mercoa.com>
2024-05-04 07:08:45 -0700

    add claude3 and gpt-4-turbo support

--- README.md
+++ README.md
@@ -14,11 +14,11 @@ Sponsored by [Mercoa](https://mercoa.com), the API for BillPay and Invoicing. Ev
 
 ---
 
-LLM Based OCR and Document Parsing for Node.js. Uses GPT4-V for OCR and data extraction.
+LLM Based OCR and Document Parsing for Node.js. Uses GPT4 and Claude3 for OCR and data extraction.
 
-- Converts PDFs (including multi page PDFs) into PNGs for use with GPT4-V
+- Converts PDFs (including multi page PDFs) into PNGs for use with GPT4
 - Automatically crops white-space to create smaller inputs
-- Cleans up JSON string returned by GPT4-V and converts it to an JSON object
+- Cleans up JSON string returned by the LLM and converts it to an JSON object
 - Custom prompt support for capturing any data you need
 
 Supports:
@@ -50,12 +50,13 @@ Note: If you are deploying via Docker, see the [Dockerfile](https://github.com/m
 import { DocumentOcr, prompts } from "llm-document-ocr";
 
 const documentOcr = new DocumentOcr({
-  apiKey: 'YOUR-OPENAI-API-KEY' // required, defaults to process.env.OPENAI_API_KEY
-  model: "gpt-4-vision-preview", // optional, defaults to "gpt-4-vision-preview"
+  apiKey: 'YOUR-OpenAi/Anthropic-API-KEY' // required, defaults to process.env.OPENAI_API_KEY. OpenAI models need an OpenAI API key, Antrhopic models need an Anthropic API key.
+  model: "gpt-4-turbo", // optional, defaults to "gpt-4-turbo". Options are: "gpt-4-turbo", "gpt-4-vision-preview", "claude-3-opus-20240229", "claude-3-sonnet-20240229", "claude-3-haiku-20240307"
   standardFontDataUrl: "https://unpkg.com/pdfjs-dist@3.2.146/standard_fonts/", // optional, defaults to "https://unpkg.com/pdfjs-dist@3.2.146/standard_fonts/". You can use the systems fonts or the fonts under ./node_modules/pdfjs-dist/standard_fonts/ as well.
 });
 
 const documentData = await documentOcr.process({
+  model: "gpt-4-turbo", // optional, defaults to "gpt-4-turbo". Options are "gpt-4-turbo" or "gpt-4-large"
   document: 'JVBERi0xLjMNCiXi48/TDQoNCjEgMCBvYmoNCjw8DQ...', // Base64 String, Base64 URI, or Buffer
   mimeType: 'application/pdf', // mime-type of the document or image
   prompt: 'invoiceStartDate, invoiceEndDate, amount', // system prompt for data extraction. See examples below.
@@ -65,7 +66,7 @@ const documentData = await documentOcr.process({
 
 ## Prompts
 
-Prompts will be automatically prefixed to tell GPT4-V to return JSON. You will need to specify the data you wish to extract, and GPT will return a JSON object with those keys.
+Prompts will be automatically prefixed to tell the LLM to return JSON. You will need to specify the data you wish to extract, and the LLM will return a JSON object with those keys.
 
 For example, the prompt we use at Mercoa for invoice processing is the following:
 
@@ -101,10 +102,6 @@ And this returns a JSON object that looks like:
   }>
 }
 ```
-
-## Limitations
-
-Currently, GPT4-V has a limitation of 100 requests per day.
 
 ## Issues and Contributing
 
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import Anthropic from "@anthropic-ai/sdk";
 import OpenAI from "openai";
 import { pdf as pdfToImg } from "pdf-to-img";
 import { cropImage } from "./cropImage";
@@ -14,17 +15,21 @@ export class DocumentOcr {
   apiKey: string;
   model: string;
   standardFontDataUrl: string;
-  openai: OpenAI;
   debug: boolean;
 
   constructor({
     apiKey = process.env.OPENAI_API_KEY,
-    model = "gpt-4-vision-preview",
+    model = "gpt-4-turbo",
     standardFontDataUrl = "https://unpkg.com/pdfjs-dist@3.2.146/standard_fonts/",
     debug = false,
   }: {
     apiKey?: string;
-    model?: "gpt-4-vision-preview";
+    model?:
+      | "gpt-4-turbo"
+      | "gpt-4-vision-preview"
+      | "claude-3-opus-20240229"
+      | "claude-3-sonnet-20240229"
+      | "claude-3-haiku-20240307";
     standardFontDataUrl?: string;
     debug?: boolean;
   }) {
@@ -34,9 +39,6 @@ export class DocumentOcr {
     this.apiKey = apiKey;
     this.model = model;
     this.standardFontDataUrl = standardFontDataUrl;
-    this.openai = new OpenAI({
-      apiKey,
-    });
     this.debug = debug;
   }
 
@@ -45,6 +47,7 @@ export class DocumentOcr {
     mimeType,
     prompt,
     pageOptions = PageOptions.ALL,
+    multiplePasses = false,
   }: {
     document: string | Buffer;
     mimeType:
@@ -56,7 +59,11 @@ export class DocumentOcr {
       | "application/pdf";
     prompt: string;
     pageOptions?: PageOptions;
+    multiplePasses?: boolean;
   }) => {
+    if (this.debug) {
+      console.time("process");
+    }
     // remove the data:image/xxx;base64, prefix if it exists
     if (typeof document === "string" && document.indexOf(",") > 0) {
       if (this.debug) console.log("Removing data:xxx/xxx;base64, prefix");
@@ -117,58 +124,50 @@ export class DocumentOcr {
       imageData.push(buffer);
     }
 
-    if (this.debug) {
-      console.log(`Sending ${imageData.length} images to LLM`);
-      for await (const image of imageData) {
-        console.log("  ~~~~ Image ~~~~ ");
-        console.log(image.toString("base64"));
-        console.log("  ~~~~ End Image ~~~~ ");
-      }
-    }
-
-    // crop images and structure into LLM input format
-    const imageGPTArray = await Promise.all(
-      imageData.map(async (image) => {
-        const message: OpenAI.Chat.Completions.ChatCompletionContentPart = {
-          type: "image_url",
-          image_url: {
-            url: await cropImage(image),
-          },
-        };
-        return message;
-      })
-    );
-
-    if (this.debug) {
-      console.log({
-        prompt,
-      });
-    }
-
-    const response = await this.openai.chat.completions.create({
-      model: this.model,
-      // response_format: {
-      //   type: 'json_object',
-      // },
-      max_tokens: 4096,
-      temperature: 0,
-      messages: [
-        {
-          role: "system",
-          content:
-            "in the images supplied find " +
-            prompt +
-            "\nRespond with a valid JSON object with all numbers as a string an no additional text or characters.",
-        },
-        {
-          role: "user",
-          content: [...imageGPTArray],
-        },
-      ],
-    });
+    // if (this.debug) {
+    //   console.log(`Sending ${imageData.length} images to LLM`)
+    //   for await (const image of imageData) {
+    //     console.log('  ~~~~ Image ~~~~ ')
+    //     console.log(image.toString('base64'))
+    //     console.log('  ~~~~ End Image ~~~~ ')
+    //   }
+    // }
 
     // clean up the response
-    let content = response.choices[0].message.content ?? "";
+    let content = "";
+
+    if (this.model.startsWith("gpt-4")) {
+      content = await useOpenAI({
+        model: this.model as "gpt-4-turbo" | "gpt-4-vision-preview",
+        imageData,
+        prompt,
+        debug: this.debug,
+        apiKey: this.apiKey,
+      });
+    } else if (
+      [
+        "claude-3-opus-20240229",
+        "claude-3-sonnet-20240229",
+        "claude-3-haiku-20240307",
+      ].includes(this.model)
+    ) {
+      if (mimeType === "application/pdf") {
+        mimeType = "image/png";
+      } else if (mimeType === "image/jpg") {
+        mimeType = "image/jpeg";
+      }
+      content = await useAnthropic({
+        imageData,
+        prompt,
+        debug: this.debug,
+        apiKey: this.apiKey,
+        mimeType,
+        model: this.model as
+          | "claude-3-opus-20240229"
+          | "claude-3-sonnet-20240229"
+          | "claude-3-haiku-20240307",
+      });
+    }
 
     if (this.debug) {
       console.log("Raw LLM Response: ");
@@ -185,8 +184,163 @@ export class DocumentOcr {
     if (this.debug) {
       console.log("Cleaned LLM Response: ");
       console.log(JSON.stringify(out, null, 2));
+      console.timeEnd("process");
     }
 
     return out;
+
+    async function useOpenAI({
+      imageData,
+      prompt,
+      debug,
+      model,
+      apiKey,
+    }: {
+      imageData: Array<Buffer>;
+      prompt: string;
+      debug: boolean;
+      model: "gpt-4-turbo" | "gpt-4-vision-preview";
+      apiKey: string;
+    }) {
+      // crop images and structure into LLM input format
+      const imageGPTArray = await Promise.all(
+        imageData.map(async (image) => {
+          const message: OpenAI.Chat.Completions.ChatCompletionContentPart = {
+            type: "image_url",
+            image_url: {
+              url: await cropImage(image),
+            },
+          };
+          return message;
+        })
+      );
+
+      if (debug) {
+        console.log({
+          prompt,
+        });
+      }
+
+      const openai = new OpenAI({
+        apiKey,
+      });
+
+      const response = await openai.chat.completions.create({
+        model,
+        response_format: {
+          type: model === "gpt-4-vision-preview" ? "text" : "json_object",
+        },
+        max_tokens: 4096,
+        temperature: 0,
+        messages: [
+          {
+            role: "system",
+            content:
+              "Find: " +
+              prompt +
+              "\nRespond with a valid JSON object with all numbers as a string an no additional text or characters.",
+          },
+          {
+            role: "user",
+            content: [...imageGPTArray],
+          },
+        ],
+      });
+      return response.choices[0].message.content ?? "";
+    }
+
+    async function useAnthropic({
+      imageData,
+      prompt,
+      debug,
+      model = "claude-3-haiku-20240307",
+      apiKey,
+      mimeType,
+    }: {
+      imageData: Array<Buffer>;
+      prompt: string;
+      debug: boolean;
+      model?:
+        | "claude-3-opus-20240229"
+        | "claude-3-sonnet-20240229"
+        | "claude-3-haiku-20240307";
+      apiKey: string;
+      mimeType: "image/png" | "image/jpeg" | "image/webp" | "image/gif";
+    }) {
+      // crop images and structure into LLM input format
+      const imageGPTArray = await Promise.all(
+        imageData.map(async (image) => {
+          const message: Anthropic.ImageBlockParam = {
+            type: "image",
+            source: {
+              type: "base64",
+              media_type: mimeType,
+              data: (await cropImage(image)).split(",")[1],
+            },
+          };
+          return message;
+        })
+      );
+
+      if (debug) {
+        console.log({
+          prompt,
+        });
+      }
+
+      const anthropic = new Anthropic({
+        apiKey,
+      });
+
+      let extractedText = "";
+      if (multiplePasses) {
+        const ocr = await anthropic.messages.create({
+          model: "claude-3-haiku-20240307",
+          max_tokens: 4096,
+          temperature: 0,
+          messages: [
+            {
+              role: "user",
+              content: [
+                {
+                  type: "text",
+                  text: "Extract all data including unlabeled fields",
+                },
+                ...imageGPTArray,
+              ],
+            },
+          ],
+        });
+
+        if (debug) {
+          console.log(ocr.content[0].text ?? "");
+        }
+        extractedText = ocr.content[0].text ?? "";
+      }
+
+      const response = await anthropic.messages.create({
+        model,
+        max_tokens: 4096,
+        temperature: 0,
+        messages: [
+          {
+            role: "user",
+            content: [
+              ...imageGPTArray,
+              {
+                type: "text",
+                text:
+                  `${extractedText}
+` +
+                  "Find: " +
+                  prompt +
+                  "\nRespond with a valid JSON object with all numbers as a string an no additional text or characters and if not provided, return null.",
+              },
+            ],
+          },
+        ],
+      });
+      return response.content[0].text ?? "";
+    }
   };
 }
